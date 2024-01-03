2c598b76b229bc46774b98264f4ccbeeed22aeff
Harshal-Dongare <myeducationalarena@gmail.com>
2024-01-03 13:16:17 +0530

    Program 2

---
+++ Array-TimeComplexity-SpaceComplexity/setAllvaluesToOne.cpp
@@ -0,0 +1,20 @@
+// set all values of an array to 1's
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+
+    // approach 1
+    int arrEle[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int size = sizeof(arrEle)/sizeof(int);
+
+    for(int index =0; index < size; index++){
+        arrEle[index] = 1;
+        cout << arrEle[index] << " ";
+    }
+
+
+    return 0;   
+}
