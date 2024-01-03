08a0917aaffb9acb29fd89998af1b7bcc245e5e7
Harshal-Dongare <myeducationalarena@gmail.com>
2024-01-03 13:20:51 +0530

    Program 3

---
+++ Array-TimeComplexity-SpaceComplexity/setAllValuesTo0and-1.cpp
@@ -0,0 +1,20 @@
+// set all values inside an array to either 0 or -1 using memset() function
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    // memset() approach:
+    // syntax: memset(arrName, -1 or 0, sizeof(arrName));
+
+    int arrEle[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int size = sizeof(arrEle)/ sizeof(int);
+    memset(arrEle, -1, sizeof(arrEle));
+
+    for(int index = 0; index < size ; index++){
+        cout << arrEle[index] << " ";
+    }
+    
+    return 0;
+}
