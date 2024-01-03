93b530b5446ffc6a1a6d73317cf0fc1c72752ce8
Harshal-Dongare <myeducationalarena@gmail.com>
2024-01-03 13:00:19 +0530

    Double the values of array

---
+++ doublesTheValues.cpp
@@ -0,0 +1,16 @@
+// Doubles the values of array and print them. Do not alter the array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arrEle[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};    
+    int size = sizeof(arrEle)/sizeof(int);    
+
+    for(int index = 0; index < size; index++){
+        cout << arrEle[index] * 2 << " ";
+        
+    }
+    cout << endl;
+    return 0;
+}
