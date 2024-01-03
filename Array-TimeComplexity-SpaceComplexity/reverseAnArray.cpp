e5f9994d35a33e7c264025e53941790950ba5f26
Harshal-Dongare <myeducationalarena@gmail.com>
2024-01-03 16:09:49 +0530

    Program 9

---
+++ Array-TimeComplexity-SpaceComplexity/reverseAnArray.cpp
@@ -0,0 +1,24 @@
+// Reverse all the elements in an array.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arrEle[] = {12, 2, 4, 35, 77, 96, 5, 99};
+    int size = 8;
+
+    int start = 0, end = size - 1;
+
+    while(start <= end){
+        if(start < end){
+            swap(arrEle[start], arrEle[end]);
+            start++;
+            end--;
+        }
+    }
+
+    for(auto element: arrEle){
+        cout << element << " ";
+    }
+    return 0;
+}
