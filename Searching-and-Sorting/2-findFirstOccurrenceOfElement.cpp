c1b3c4b4f02b21c3e552cfa960a4dc9b743dcd4e
Harshal-Dongare <myeducationalarena@gmail.com>
2024-01-06 23:17:54 +0530

    [Binary S] New approach added

--- Searching-and-Sorting/2-findFirstOccurrenceOfElement.cpp
+++ Searching-and-Sorting/2-findFirstOccurrenceOfElement.cpp
@@ -6,8 +6,10 @@ output: 1st index
 
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
+// Approach 1 function
 int firstOccurrence(vector<int> arr, int target){
     int start = 0;
     int end = arr.size() - 1;
@@ -32,10 +34,25 @@ int firstOccurrence(vector<int> arr, int target){
 }
 
 int main(){
+    // Note: Run one approach at a time instead of running entire program at once
+
+    // Approach 1: user-defined find first occurence method
     vector<int> arrEle= {1, 2, 3, 8, 8, 9, 12, 15};
     int target = 8;
 
     int ans = firstOccurrence(arrEle, target);
     cout << ans << " index" << endl;
+
+//-------------------------------------------------------------------------------------
+
+    // Approach 2: We have predefined STL method available to find first occurrence of the element in <>
+    vector<int> arrEle2= {1, 2, 3, 8, 8, 9, 12, 15};
+    int target = 8;
+
+    // lower_bound stl
+    auto ans2 = lower_bound(arrEle2.begin(), arrEle2.end(), target);
+
+    cout << ( ans2 - arrEle2.begin() ) << endl;
+
     return 0;
 }
