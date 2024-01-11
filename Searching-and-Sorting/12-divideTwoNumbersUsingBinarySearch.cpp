14418dd8e61af2fde5525b51aceff5c7ee0f3815
Harshal-Dongare <myeducationalarena@gmail.com>
2024-01-11 15:03:41 +0530

    [Binary S] Divide two numbers using BS

---
+++ Searching-and-Sorting/12-divideTwoNumbersUsingBinarySearch.cpp
@@ -0,0 +1,65 @@
+/*
+Problem statement: Find the quotient by divinding two numbers using binary search
+*/
+
+#include<iostream>
+using namespace std;
+
+int findQuotient(int dividend, int divisor){
+    int start = 0;
+    int end = abs(dividend);
+    int ans = 0;
+
+    int mid = start + (end -start)/2;
+
+    while(start <= end){
+        // while solving the problem take all values as positive using abs function
+
+        // perfect answer
+        if(abs(mid*divisor) == abs(dividend)){
+            ans = mid;
+            break;
+        }
+        // search left if mid*divisor is greater than the dividend
+        if(abs(mid*divisor) > abs(dividend)) end = mid -1; 
+        if(abs(mid*divisor) < abs(dividend)){
+            ans = mid;           // store the answer
+            start = mid + 1;     // search right
+        }
+        // update mid
+        mid = start + (end -start)/2;
+    }
+
+    // if both dividend and divisor is poistive or negative
+    if((dividend < 0) && (divisor < 0) || (dividend > 0) && (divisor > 0)){
+        return ans;
+    }
+    else{
+        // if any one of the value is negative
+        return -ans;
+    }
+};
+
+int main(){
+    int dividend = 22;
+    int divisor = 7;
+
+    int ans = findQuotient(dividend, divisor);
+
+    // for precision 
+    double finalAnwser = ans;
+    int precision;
+    cout << "Enter the precision count: ";
+    cin >> precision;
+    double step = 0.1;
+
+    for(int i = 0; i < precision; i++){
+        for(double j = ans; j*divisor <= dividend; j = j + step){
+            finalAnwser = j;
+        }
+        step = step/10;
+    }
+
+    cout << "The answer is: " << finalAnwser << endl;
+    return 0;
+}
