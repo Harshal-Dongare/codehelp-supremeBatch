0d104627c7ef1f22cd16f6c4138aa8949800c474
Harshal Dongare <myeducationalarena@gmail.com>
2024-01-14 14:03:39 +0530

    notes added regarding memset() function

--- Array-TimeComplexity-SpaceComplexity/setAllValuesTo0and-1.cpp
+++ Array-TimeComplexity-SpaceComplexity/setAllValuesTo0and-1.cpp
@@ -20,3 +20,20 @@ int main(){
     
     return 0;
 }
+
+
+/*
+Why memset() doesn't print expected output for integers like 42, -74, 12, 60 ?
+
+-> memset() function sets each byte of the memory block to a given value. When you use it to set integer byte, the interpretation of those bytes depends on the endianess of the system. 
+
+-> 1. Little-endian system: The least byte of the binary number of a decimal is stored at the lowest memory address. 
+    example: let's you have 32bit integer format
+    42: 00101010 
+    representation of 42 in 32 bit integer format of Little-endian system will be : 00101010 00000000 00000000 00000000 
+
+    this is how each memory block of array get set with this value and when you print each value, compiler again convert this value to number but it converts to 707406378 which is the conversion of 
+    00101010 00000000 00000000 00000000. It is totally depend upon the endianess of the system
+    
+-> 2. Big-endian system: The most significant byte of the binary number is stored at lowest memory address.
+*/
