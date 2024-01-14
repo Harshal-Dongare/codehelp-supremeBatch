c52a5aa2439cd268de53fbb3d7b2d8335b177177
Harshal Dongare <myeducationalarena@gmail.com>
2024-01-14 23:22:46 +0530

    [format] function return type changed

--- Array-TimeComplexity-SpaceComplexity/searchElementIn2DArray.cpp
+++ Array-TimeComplexity-SpaceComplexity/searchElementIn2DArray.cpp
@@ -3,22 +3,24 @@
 #include<iostream>
 using namespace std;
 
-bool findElement(int arr[][3], int rows, int cols, int key){
+int findElement(int arr[][3], int rows, int cols, int key){
 
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
             // if element founds, return true
-            if(key == arr[i][j]) return true;
+            if(key == arr[i][j]) return cols*i+j;
         }
     }
-    return false;
+    return -1;
 }
 
 int main(){
     int arrEle[3][3] = {{41, 23, 52},{77, 11, 66},{25, 79, 12}};
     int rows = 3, cols = 3, key = 53;
 
-    if(findElement(arrEle, rows, cols, key)) cout << "element found";
+    int ans = findElement(arrEle, rows, cols, key);
+    
+    if(ans != -1) cout << "element found at index: " << ans;
     else cout << "Element not found";
 
     return 0;
